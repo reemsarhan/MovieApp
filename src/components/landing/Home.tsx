00baa6f0b2315bc764bda78bcd955b91cd6353c4
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2025-05-06 15:26:50 +0300

    last edits

--- src/components/landing/Home.tsx
+++ src/components/landing/Home.tsx
@@ -1,84 +1,4 @@
-// "use client";
-
-// import { useState } from "react";
-// import { getDefaultMovies } from "@/services/movie";
-// import MovieCard from "@/components/movieCard/MovieCard";
-// import Search from "@/components/search/Search";
-// import styles from "@/components/landing/page.module.css";
-// import { Notification } from "@/components/movieDetails/Notifications";
-// import Link from "next/link";
-
-// import { Movie } from "@/types/movie";
-
-// export default function Home({initialMovies,initialTotalResults,}: {initialMovies: Movie[];initialTotalResults: number;}) {
-//   const [defaultMovies, setDefaultMovies] = useState<Movie[]>(initialMovies);
-//   const [page, setPage] = useState(1);
-//   const [loading, setLoading] = useState(false);
-//   const [searchResults, setSearchResults] = useState<Movie[]>([]);
-//   const [isDarkMode, setIsDarkMode] = useState(true);
-
-//   const loadMoreMovies = async () => {
-//     setLoading(true);
-//     const nextPage = page + 1;
-//     const data = await getDefaultMovies(nextPage);
-//     setDefaultMovies((prevMovies) => [...prevMovies, ...data.Search]);
-//     setPage(nextPage);
-//     setLoading(false);
-//   };
-
-//   const updateSearchResults = (movies: Movie[]) => {
-//     setSearchResults(movies);
-//   };
-
-//   return (
-//     <div className={`${styles.container} ${isDarkMode ? styles.dark : styles.light}`} >
-//       <nav className={styles.navBar}>
-//         <div className={styles.navLinks}>
-//           <Link href="/" className={styles.navLink}>
-//             Home
-//           </Link>
-//           <Link href="/favourites" className={styles.navLink}>
-//             Favorites
-//           </Link>
-//         </div>
-//         <div className={styles.themeToggleContainer}>
-//           <button onClick={() => setIsDarkMode((prev) => !prev)} className={styles.themeToggle}>
-//             {isDarkMode ? "☀️" : "🌙"}
-//           </button>
-//         </div>
-//       </nav>
-//       <h1>Movie Library</h1>
-//       <Notification />
-//       <Search updateChangeResults={updateSearchResults} searchResults={searchResults}/>
-//       {/* conditional rendering */}
-//       {
-//         searchResults.length === 0 && (
-//           <div className={styles.movieList}>
-//             {defaultMovies.length > 0 ? (
-//               defaultMovies.map((movie) => (
-//                 <MovieCard key={movie.imdbID} movie={movie} />
-//               ))
-//             ) : (
-//               <p>No movies found.</p>
-//             )}
-//           </div>
-//         )
-//       }
-//       {
-//         searchResults.length === 0 &&
-//         defaultMovies.length > 0 &&
-//         defaultMovies.length < initialTotalResults && (
-//           <button onClick={loadMoreMovies} className={styles.loadMoreButton} disabled={loading}>
-//             {loading ? "Loading..." : "Load More"}
-//           </button>
-//         )
-//       }
-//     </div>
-//   );
-// }
-
-
- "use client";
+"use client";
 
 import { useState } from "react";
 import { getDefaultMovies } from "@/services/movie";
@@ -125,6 +45,7 @@ export default function Home({initialMovies,initialTotalResults,}: {initialMovie
           <Link href="/favourites" className={styles.navLink}>
             Favorites
           </Link>
+          
         </div>
 
         {/* Hamburger Menu Icon */}
